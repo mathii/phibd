897bd65f2f567e215c09ebcda562667e710f13b9
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2016-05-31 17:48:12 -0400

    try and deal with low marker numbers

--- analyse_test_results.R
+++ analyse_test_results.R
@@ -7,12 +7,14 @@ dd2 <-  ifelse(data$ID1<data$ID2, data$ID2, data$ID1)
 data$ID1 <- dd1
 data$ID2 <- dd2
 
+N_cutoff<-1e5
+
 rel <- read.table("~/phibd/Mathieson_etal_rel", as.is=TRUE)
 
 rels <- paste0(data$ID1, data$ID2) %in% paste0(rel[,1], rel[,2])
 
 pdf("~/phibd/p_vs_10mblength.pdf")
-plot(data$p, data$IBD_filtered, pch=16, col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Pseudo-haploid heterozygosity", ylab="Proportion of genome in IBD chunks >10mb")
+plot(data$p, data$IBD_filtered, pch=ifelse(data$N_SNP<N_cutoff, 1, 16), col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Pseudo-haploid heterozygosity", ylab="Proportion of genome in IBD chunks >10mb")
 legend("topright", c("Related", "Unrelated"), col=c("#E41A1C80" , "#377EB840"), pch=16, bty="n")
 abline(h=0.75, col="grey", lty=2)
 abline(h=0.5, col="grey", lty=2)
@@ -21,12 +23,12 @@ abline(h=0.125, col="grey", lty=2)
 dev.off()
 
 pdf("~/phibd/SNPS_v_length.pdf", width=6, height=6)
-plot(data$N_SNP, data$IBD, col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Number of SNPs", ylab="Proportion of genome in IBD chunks", pch=16)
+plot(data$N_SNP, data$IBD, col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Number of SNPs", ylab="Proportion of genome in IBD chunks", pch=ifelse(data$N_SNP<N_cutoff, 1, 16))
 points(data$N_SNP[rels], data$IBD[rels], col="#E41A1C80")
 legend("topleft", c("Related", "Unrelated"), col=c("#E41A1C80" , "#377EB840"), pch=16, bty="n")
 dev.off()
 pdf("~/phibd/SNPS_v_10mblength.pdf", width=6, height=6)
-plot(data$N_SNP, data$IBD_filtered, col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Number of SNPs", ylab="Proportion of genome in IBD chunks > 10mb", pch=16)
+plot(data$N_SNP, data$IBD_filtered, col=ifelse(rels, "#E41A1C80" , "#377EB840"), xlab="Number of SNPs", ylab="Proportion of genome in IBD chunks > 10mb", pch=ifelse(data$N_SNP<N_cutoff, 1, 16))
 points(data$N_SNP[rels], data$IBD_filtered[rels], col="#E41A1C80")
 
 legend("topright", c("Related", "Unrelated"), col=c("#E41A1C80" , "#377EB840"), pch=16, bty="n")
--- phibd.py
+++ phibd.py
@@ -27,6 +27,8 @@ def parse_options():
                         "List of pairs to test (default individuals*individuals)")
     parser.add_argument('-m', '--min_chunk', type=float, default=10.0, help=
                         "Filtered results remove chunks less than this size")
+    parser.add_argument('-r', '--min_markers', type=int, default=1000, help=
+                        "Only include pairs with at least this many markers in common")
     parser.add_argument('-n', '--ncore', type=int, default=1, help=
                         "number of cores to parallelize IBD computation")
     parser.add_argument('-t', '--max_iters', type=int, default=15, help=
@@ -84,6 +86,9 @@ def get_job(data, pair, options):
         pos=data.snp["POS"][include][nonmissing]
         job["chr"+chrom]={"states":states, "pos":pos}
     
+    job["total_markers"]=sum([len(job["chr"+chrom]) for chrom in  options.chromosomes])
+    job["min_markers"]=min([len(job["chr"+chrom]) for chrom in  options.chromosomes])
+    
     return job
     
 ################################################################################
@@ -94,6 +99,7 @@ def make_jobs(data, options):
     """
     pairs=make_pairs(data, options)
     jobs=[get_job(data, x, options) for x in pairs]
+    jobs=[j for j in jobs if j["min_markers"]>=options.min_markers]
     return jobs
     
 ################################################################################
